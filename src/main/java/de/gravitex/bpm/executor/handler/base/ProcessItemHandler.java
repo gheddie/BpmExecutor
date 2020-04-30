8573a8c4454ac027e3f2f9ebefad46b0abd4ee19
Sts <Sts@YellowMeerkat.sternico.local>
2020-04-30 12:40:49 +0200

    123

--- src/main/java/de/gravitex/bpm/executor/handler/base/ProcessItemHandler.java
+++ src/main/java/de/gravitex/bpm/executor/handler/base/ProcessItemHandler.java
@@ -1,14 +1,10 @@
 package de.gravitex.bpm.executor.handler.base;
 
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.camunda.bpm.engine.ManagementService;
-import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.camunda.bpm.engine.task.Task;
 
 import de.gravitex.bpm.executor.app.BpmExecutionSingleton;
 import de.gravitex.bpm.executor.exception.BpmExecutorException;
@@ -39,25 +35,6 @@ public abstract class ProcessItemHandler<T> {
 
 	protected RuntimeService runtimeService() {
 		return BpmExecutionSingleton.getInstance().getProcessEngine().getRuntimeService();
-	}
-	
-	// ------------------------------------------------------------------------------------
-	// --------- asserts
-	// ------------------------------------------------------------------------------------
-	
-	protected void assertTaskPresent(String taskDefinitionKey, int expectedTaskCount, ProcessEngine processEngine) throws BpmExecutorException {
-		
-		List<Task> taskList = processEngine.getTaskService().createTaskQuery().taskDefinitionKey(taskDefinitionKey).list();
-		if (!(taskList.size() == expectedTaskCount)) {
-			throw new BpmExecutorException("invalid count for task [" + taskDefinitionKey + "], expected: " + expectedTaskCount
-					+ ", found: " + taskList.size() + "!!", null);
-		}
-	}
-	
-	protected void assertRunningProcesses(int processCount, ProcessEngine processEngine) throws BpmExecutorException {
-		if (!(processEngine.getRuntimeService().createExecutionQuery().list().size() == processCount)) {
-			throw new BpmExecutorException("invalid count for task [" + processCount + "]!!", null);			
-		}
 	}
 	
 	protected String formatForProcessInstance(String message, ProcessInstance processIstance) {
