e68cad00ab00d279ff54dd9586c7e1d8d881f0af
Sts <Sts@YellowMeerkat.sternico.local>
2020-05-02 17:23:20 +0200

    123

--- src/main/java/de/gravitex/bpm/executor/app/BpmExecutionMain.java
+++ src/main/java/de/gravitex/bpm/executor/app/BpmExecutionMain.java
@@ -1,5 +1,8 @@
 package de.gravitex.bpm.executor.app;
 
+import de.gravitex.bpm.executor.checker.TaskT1BpmChecker;
+import de.gravitex.bpm.executor.checker.TaskTxBpmChecker;
+import de.gravitex.bpm.executor.handler.TaskT1Handler;
 import de.gravitex.bpm.executor.handler.TaskTMHandler;
 import de.gravitex.bpm.executor.settings.ProcessExecutorSettings;
 
@@ -9,18 +12,14 @@ public class BpmExecutionMain {
 
 	public static void main(String[] args) {
 		try {
-			
-			/*
-			BpmExecutionSingleton.getInstance()
-					.handleProcessExecutor(ProcessExecutor.create("SimpleTestProcess.bpmn", "SimpleTestProcess")
-							.withCustomHandler("TASK#T1", new TaskT1Handler()).withBpmStateChecker("TASK#T1", new TaskT1BpmChecker())
-							.withSettings(DEFAULT_SETTINGS));
-			BpmExecutionSingleton.getInstance().handleProcessExecutor(ProcessExecutor.create("AnotherProcess.bpmn", "AnotherProcess")
+			BpmExecutionSingleton instance = BpmExecutionSingleton.getInstance();
+			instance.handleProcessExecutor(
+					ProcessExecutor.create("SimpleTestProcess.bpmn", "SimpleTestProcess").withCustomHandler("TASK#T1", new TaskT1Handler())
+							.withBpmStateChecker("TASK#T1", new TaskT1BpmChecker()).withSettings(DEFAULT_SETTINGS));
+			instance.handleProcessExecutor(ProcessExecutor.create("AnotherProcess.bpmn", "AnotherProcess")
 					.withBpmStateChecker("TASK#TX", new TaskTxBpmChecker()).withSettings(DEFAULT_SETTINGS));
-					*/
-			
-			BpmExecutionSingleton.getInstance().handleProcessExecutor(ProcessExecutor.create("LoopProcess.bpmn", "LoopProcess").withCustomHandler("TASK#TM", new TaskTMHandler()));
-			
+			instance.handleProcessExecutor(
+					ProcessExecutor.create("LoopProcess.bpmn", "LoopProcess").withCustomHandler("TASK#TM", new TaskTMHandler()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/de/gravitex/bpm/executor/gui/BpmGui.java
@@ -0,0 +1,85 @@
+/*
+ * Created by JFormDesigner on Sat May 02 17:15:10 CEST 2020
+ */
+
+package de.gravitex.bpm.executor.gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+import de.gravitex.bpm.executor.app.BpmExecutionSingleton;
+import de.gravitex.bpm.executor.app.ProcessExecutor;
+import de.gravitex.bpm.executor.checker.TaskT1BpmChecker;
+import de.gravitex.bpm.executor.checker.TaskTxBpmChecker;
+import de.gravitex.bpm.executor.handler.TaskT1Handler;
+import de.gravitex.bpm.executor.handler.TaskTMHandler;
+
+/**
+ * @author Stefan Schulz
+ */
+public class BpmGui extends JFrame {
+	
+	private static final long serialVersionUID = -8277137714910541545L;
+	
+	public BpmGui() {
+		initComponents();
+	}
+	
+	public static void main(String[] args) {
+	
+		try {
+			BpmExecutionSingleton.getInstance()
+					.handleProcessExecutor(ProcessExecutor.create("SimpleTestProcess.bpmn", "SimpleTestProcess")
+							.withCustomHandler("TASK#T1", new TaskT1Handler()).withBpmStateChecker("TASK#T1", new TaskT1BpmChecker()));
+			BpmExecutionSingleton.getInstance().handleProcessExecutor(ProcessExecutor.create("AnotherProcess.bpmn", "AnotherProcess")
+					.withBpmStateChecker("TASK#TX", new TaskTxBpmChecker()));
+			BpmExecutionSingleton.getInstance().handleProcessExecutor(ProcessExecutor.create("LoopProcess.bpmn", "LoopProcess").withCustomHandler("TASK#TM", new TaskTMHandler()));
+			
+			BpmGui bpmGui = new BpmGui();
+			bpmGui.setSize(900, 600);
+			bpmGui.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - Stefan Schulz
+		scrollPane1 = new JScrollPane();
+		list1 = new JList();
+		button1 = new JButton();
+
+		//======== this ========
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new GridBagLayout());
+		((GridBagLayout)contentPane.getLayout()).columnWidths = new int[] {124, 0, 0};
+		((GridBagLayout)contentPane.getLayout()).rowHeights = new int[] {0, 0, 0};
+		((GridBagLayout)contentPane.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0E-4};
+		((GridBagLayout)contentPane.getLayout()).rowWeights = new double[] {1.0, 0.0, 1.0E-4};
+
+		//======== scrollPane1 ========
+		{
+			scrollPane1.setViewportView(list1);
+		}
+		contentPane.add(scrollPane1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 5, 5), 0, 0));
+
+		//---- button1 ----
+		button1.setText("Start process");
+		contentPane.add(button1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 0, 5), 0, 0));
+		pack();
+		setLocationRelativeTo(getOwner());
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - Stefan Schulz
+	private JScrollPane scrollPane1;
+	private JList list1;
+	private JButton button1;
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
+}
